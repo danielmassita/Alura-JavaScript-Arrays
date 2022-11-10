2afaa374f446bc3a566da71e8706bee113205f5f
danielmassita <daniel.massita@gmail.com>
2022-11-10 00:09:11 -0300

    Fim da Unidade 01

---
+++ Unidade01-Aula09.js
@@ -0,0 +1,26 @@
+/*
+
+09 - Jantar de Hoje! 
+
+O push e pop são usados para adicionar/remover elementos de uma lista, como mostra o seguinte código:
+
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)COPIAR CÓDIGO
+Analisando o código, selecione a alternativa que corresponde ao que teremos como saída do console do jantarDeHoje .
+
+Selecione uma alternativa
+
+A - 
+[ '🍔', '🍳', '🥗', '🍏' ]
+
+Alternativa correta! Certo! No jantar de hoje, a pizza e o cachorro quente não estão presentes por conta do código jantarDeHoje.pop(), que remove o último elemento da lista e o ovo, a salada e a maçã verde entraram na lista com comando push.
+
+*/
---
+++ Unidade01-Aula10.js
@@ -0,0 +1,8 @@
+/*
+Nesta aula vimos:
+As características principais de arrays;
+Como funciona uma lista ordenada;
+Os conceitos de elemento, posição e índice;
+Como identificar a quantidade de elementos em um array com .length;
+Como fazer as primeiras alterações em um array com .push() e .pop().
+*/
