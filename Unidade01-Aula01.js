9119c8580dd2572a2ef119b8fdd157992cb35bc8
danielmassita <daniel.massita@gmail.com>
2022-11-09 23:16:31 -0300

    Primeiro commit pra começar os estudos...

--- Unidade01-Aula01.js
+++ Unidade01-Aula01.js
@@ -1,1 +1,17 @@
+/*
+Transcrição
+[00:00] Leonardo: Boas-vindas ao curso de JS, nesse curso vamos começar a trabalhar um pouco mais com as estruturas fundamentais da programação, no caso os arrays. Esse curso está voltado para quem começou a programar recentemente e escolheu o Java script como uma das suas primeiras linguagens de programação. Lembrando que esse curso não vai abordar nenhum tópico específico do frontend, ele é mais focado no backend, porém os conceitos servem para ambos os casos.
+
+[00:30] Leonardo: De onde vamos começar a ver, qual conteúdo vamos começar vendo? Vamos ver sobre os arrays, quais são as estruturas, suas partes, como fazemos para guardar dados dentro deles, e para recuperar esses dados depois. Vamos começar com alguns casos práticos para ver como essa estrutura é formada.
+
+[00:52] Juliana: E vamos ver também alguns métodos nativos que o Java script tem para trabalhar com arrays, então vamos utilizar alguns dos métodos mais comuns para acessar valores, separar valores, incluir, deletar, entre outras ferramentas para entender melhor como trabalhamos com essa estrutura de dados no dia a dia.
+
+[01:14] Leonardo: Em seguida, vamos ver sobre os loops, ou laços de repetição. Eles são uma parte muito importante da programação e nós vamos utilizar muito com arrays, para passar pelo array inteiro e fazer uma conta ou pegar um valor lá dentro, assim por diante. Vamos ver quando utilizar e como utilizar eles. Tanto com for, que existe em várias linguagens, como com alguns mais específicos, como foreach e o map.
+
+[01:42] Juliana: O Java script a partir da sua versão 2015, o ES6, acrescentou na linguagem alguns métodos de array um pouco mais específicos, mais modernos, eles utilizam as tais das funções call-back, vamos praticar e entender como esses métodos funcionam, e como podemos utilizar esses métodos mais modernos com essas funções call-back para continuar trabalhando com arrays e iterando e fazendo todas as operações que precisamos.
+
+[02:15] Leonardo: O que você vai precisar saber antes de poder começar esse curso? É bom que você já tenha feito o outro curso de fundamentos de Java script que passa sobre tipos, variáveis e básico de funções, porque vamos utilizar alguns conceitos de lá nesse curso aqui. Como, por exemplo, arrow function, parâmetros e operadores. Lembrando mais uma vez que esse curso não trata de nada específico sobre o frontend, mas dá para aproveitar os conceitos para lá também.
+
+[02:44] Juliana: E o que você pode fazer depois desse curso? Aí você já vai poder estar apto ou apta para fazer seus projetos em Java script e resolver exercícios de algoritmos que envolvam arrays e laços de repetição. A ideia aqui é entender bem o conceito dessas partes fundamentais que chamamos do Java script, entre elas o array, para que esse conhecimento não fique faltando, fique bem completo lá na frente quando você estiver incrementando, aumentando seus conhecimentos e sua prática na linguagem. Vamos lá, vamos começar.
+*/
 
