bce6f1f96df063c1f5af7afecd5c7c7432db57da
danielmassita <daniel.massita@gmail.com>
2022-11-11 17:54:46 -0300

    Retomando os estudos...

---
+++ Unidade02-Aula05.js
@@ -0,0 +1,12 @@
+// 05 - CONECTANDO ARRAYS 
+
+// Desafio: Juntando as Salas - Haverá uma palestra sobre padrões de projetos para as alunas das salas de JavaScript e Python. Junte ambas as salas em uma única lista que exiba todas as alunas. 
+
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo'];
+const salaDeJavaScript = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript); // O .concat() não altera a lista original...
+console.log(salasUnificadas);
+console.log(salaDePython);
+console.log(salaDeJavaScript);
+
