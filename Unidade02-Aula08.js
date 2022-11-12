c8b5e39317a3c179ad67df7571e27537dbf4e859
danielmassita <daniel.massita@gmail.com>
2022-11-12 02:29:07 -0300

    Finalizando Unidade 02

---
+++ Unidade02-Aula08.js
@@ -0,0 +1,76 @@
+// 08 - Concatenando Arrays e Dados
+
+/*
+Durante a aula usamos o método concat() para unir (concatenar) arrays gerando um novo com o resultado da concatenação. Neste caso, o método funcionou da seguinte forma:*/
+
+var lista1 = ['A', 'B'];
+var lista2 = ['C', 'D'];
+var novaLista = lista1.concat(lista2);
+console.log(novaLista)
+
+/*
+Logo, o método foi utilizado com um array (no caso acima, array1) e recebeu como parâmetro outro array (array2). O resultado foi um novo array com todos os elementos de array1 e array2; isso acontece pois, quando o concat() recebe um array como parâmetro, são apenas os elementos dentro dele que são concatenados, e não o array em si.
+
+E se tentarmos usar esse método passando como parâmetro outro tipo de dado, que não seja array? Abra um arquivo index.js em seu computador e execute o seguinte código:
+*/
+
+const listaOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"];
+const listaConcat = listaOriginal.concat("André", "Fernanda");
+
+console.log(listaConcat);
+console.log(listaOriginal);
+
+/*
+O resultado deve ser:
+
+[ 'Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda' ]
+[ 'Maria', 'Carlos', 'Eduardo', 'Samanta' ]COPIAR CÓDIGO
+Com o exemplo acima, vemos que o JavaScript concatena o array principal e os números passados como parâmetro para criar outro array, mantendo o original sem alterações.
+
+Vamos fazer um novo teste, mas passando mais de um array como parâmetro.
+*/
+
+const listinhaOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const listinhaConcat = listinhaOriginal.concat(["André", "Fernanda"], ["Ricardo", "Ana"], ["Marcelo", "Bia"])
+
+console.log(listinhaConcat)
+console.log(listinhaOriginal);
+
+/*
+O resultado é:
+
+[ 'Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda', 'Ricardo', 'Ana', 'Marcelo', 'Bia' ]
+[ 'Maria', 'Carlos', 'Eduardo', 'Samanta' ]COPIAR CÓDIGO
+Podemos passar mais de um array como parâmetro para que seus elementos sejam concatenados em um único array, junto com o que está sendo chamado com o método (no caso, arrayOriginal.
+
+Como um último teste, vamos ver o que acontece se um dos parâmetros for um array com dois elementos, um número e um array:
+*/
+
+let listonaOriginal = [50, 60, 70]
+let listonaConcat = listonaOriginal.concat([80, [90, 100]])
+
+console.log(listonaConcat);
+console.log(listonaOriginal);
+
+/*
+O resultado no terminal agora é:
+
+[ 50, 60, 70, 80, [ 90, 100 ] ]
+[ 50, 60, 70 ]COPIAR CÓDIGO
+Vimos anteriormente que, quando recebe um array como parâmetro, concat() vai concatenar apenas os elementos. Porém, este método não extrai os elementos do array de forma recursiva; ou seja, não vai extrair os elementos de arrays que estejam “aninhados”. Dessa forma, 80 foi extraído do array com sucesso, porém 90 e 100 não, o método considerou [90, 100] como um único elemento.
+
+concat() é um método útil quando não se deseja alterar o array original, e sim fazer uma cópia alterada. Caso isso não seja necessário, considere utilizar push() ou splice() para inserir novos elementos ou fazer alterações no array original.
+
+
+09
+O que aprendemos?
+PRÓXIMA ATIVIDADE
+
+Nesta aula aprendemos como:
+Separar um array em partes com slice();
+Remover e incluir novos elementos em um array com splice();
+Concatenar elementos em um único array com concat();
+Trabalhar com arrays de duas dimensões.
+
+
+*/
